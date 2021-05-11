487d896dc72563f6f9c6c31db464f3edd6bac929
Kseniia <kshamtova22@mail.ru>
2021-05-11 11:42:42 +0300

    yes

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        int account = 2000000000;
+        int withdrawal = 500000000;
+        int total = account + withdrawal;
+        System.out.println(total);
+    }
+}
